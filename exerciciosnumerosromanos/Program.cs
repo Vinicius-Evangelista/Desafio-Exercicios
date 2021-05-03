1decd4ca116d584360a02f31a30e015e1c1ffa24
Vinicius <vini.evangelista@live.com>
2021-05-03 16:18:29 -0300

    desafios vogal e algarismos romanos

---
+++ exerciciosnumerosromanos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace exerciciosnumerosromanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor preencha o espaço com um numero com um algarismo romano !");
+            string algarismoRomanos = Console.ReadLine().ToUpper();
+
+            switch (algarismoRomanos)
+            {
+                case "I":
+                     Console.WriteLine("R: = 1");
+                    break;
+                
+                case "II":
+                    Console.WriteLine("R: = 2");
+                    break;
+                
+                case "III":
+                    Console.WriteLine("R: = 3");
+                    break;
+                
+                case "IV":
+                     Console.WriteLine("R: = 4");
+                    break;
+                
+                case "V":
+                     Console.WriteLine("R: = 5");
+                    break;
+                
+                case "VI":
+                     Console.WriteLine("R: = 6");
+                    break;
+                
+                case "VII":
+                     Console.WriteLine("R: = 7");
+                    break;
+                
+                case "VIII":
+                    Console.WriteLine("R: = 8");
+                    break;
+                
+                case "IX":
+                     Console.WriteLine("R: = 9");
+                    break;
+                
+                case "X":
+                     Console.WriteLine("R: = 10");
+                    break;
+               
+                default:
+                    Console.WriteLine("Por favor escreva um algarismo romano."+ "\n");
+                    Console.WriteLine("Exemplo: I, II, III, IV, V, VI, VII, VIII, IX, X." + "\n");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ exerciciovogal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exerciciovogal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome ?");
+            string nome = Console.ReadLine();
+            string letra= nome.Substring(0,1).ToLower();
+
+            switch (letra)
+            {
+                case "a":
+                    Console.WriteLine("Seu nome começa com "+ letra + " de Avelã !");
+                    break;
+
+                case "e":
+                    Console.WriteLine("Seu nome começa com " + letra + " de Êma !");
+                    break;
+
+                case "i":
+                    Console.WriteLine("Seu nome começa com " + letra + " de Impala !");
+                    break;
+
+                case "o":
+                    Console.WriteLine("Seu nome começa com " + letra + " de Onça !");
+                    break;
+
+                default:
+                    Console.WriteLine("Seu nome começa com começa com uma consoante :(");
+                    break;
+            }
+            
+        }
+    }
+}
